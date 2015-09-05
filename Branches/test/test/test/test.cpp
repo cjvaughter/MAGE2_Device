19bab7a46daa9823c01f06341173a53b5e2b61a7
sstandfast <sstandfast@yahoo.com>
2015-09-05 10:45:40 -0500

    initial setup test
    setup test

---
+++ Branches/test/test/test/test.cpp
@@ -0,0 +1,21 @@
+/*
+ * test.cpp
+ *
+ * Created: 9/5/2015 10:30:17 AM
+ *  Author: Shawn
+ */ 
+
+
+#include <avr/io.h>
+#include <stdio.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+		printf("Hello World!");
+    }
+	
+	return 0;
+}
